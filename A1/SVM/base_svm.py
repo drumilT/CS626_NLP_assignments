fcba5f9c61344a7bd581515b2e55c63c6e05b38e
drumilT <drumiltrivedi@gmail.com>
2020-09-15 06:26:26 +0530

    add basic svm

---
+++ A1/SVM/base_svm.py
@@ -0,0 +1,29 @@
+import numpy as np
+#import torch
+#from tqdm import tqdm 
+
+class base_SVM:
+    def __init__(self,tag_name):
+        print("Created SVM for tag"+tag_name)                                 
+    
+    def fit(self, data, lr=2e-5 ):
+        self.w = np.random.uniform(size=(data.shape[1]-1))
+        self.b = np.random.uniform()
+        self.lr= lr
+        labels = 2*data[:,-1]-1
+        features = data[:,:-1]
+        incorrect = np.sign(np.dot(np.array(features),self.w)+self.b) != labels
+        update = (labels*incorrect).T @ features 
+        self.w += self.lr*update 
+        self.b += self.lr*np.sum(incorrect)
+        
+    def predict(self,features):
+        classification = np.dot(np.array(features),self.w)+self.b
+        return classification
+
+
+if __name__=="__main__":
+    SNM = base_SVM()
+    data = np.array([[1,7,1],[2,8,1],[3,8,1],[5,1,0],[6,-1,0],[7,3,0]])
+    SNM.fit(data)
+
